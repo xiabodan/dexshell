fc3f715f617b710704f25270e52247170601d8cc
xiabo <xiabodan@163.com>
2021-08-28 14:58:37 +0800

    debug hellowwork success.

--- HelloWorld/app/src/main/java/org/nisosaikou/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/org/nisosaikou/helloworld/MainActivity.java
@@ -2,7 +2,13 @@ package org.nisosaikou.helloworld;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.ArrayMap;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.lang.ref.WeakReference;
+import java.util.Iterator;
 
 public class MainActivity extends Activity {
 
@@ -10,5 +16,33 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("xiabo", "MainActivity onCreate " + savedInstanceState);
+        Button button = findViewById(R.id.button);
+        Log.i("xiabo", "MainActivity onCreate button " + button);
+        if (button != null) {
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.i("xiabo", "MainActivity onClicked");
+                }
+            });
+        }
+
+        // 配置动态加载环境
+        //反射获取主线程对象，并从中获取所有已加载的package信息，并中找到当前的LoadApk对象的弱引用
+        Object currentActivityThread = RefInvoke.invokeStaticMethod("android.app.ActivityThread", "currentActivityThread", new Class[] {}, new Object[] {});
+        String packageName = this.getPackageName();
+        ArrayMap mPackages = (ArrayMap) RefInvoke.getFieldOjbect("android.app.ActivityThread", currentActivityThread, "mPackages");
+        Iterator<Object> it = mPackages.keySet().iterator();
+        while (it.hasNext()) {
+            String key = (String) it.next();
+            WeakReference weakReference = (WeakReference) mPackages.get(key);
+            Log.i("xiabo", key + ", " + weakReference.get());
+        }
+    }
+
+    private void onClick(View view) {
+        Button button = findViewById(R.id.button);
+        Log.i("xiabo", "MainActivity onClick button " + button);
     }
 }
--- HelloWorld/app/src/main/java/org/nisosaikou/helloworld/XApplication.java
+++ HelloWorld/app/src/main/java/org/nisosaikou/helloworld/XApplication.java
@@ -1,10 +1,19 @@
 package org.nisosaikou.helloworld;
 
 import android.app.Application;
+import android.content.Context;
+import android.util.Log;
 
 public class XApplication extends Application {
     @Override
+    protected void attachBaseContext(Context base) {
+        Log.i("xiabo", "XApplication attachBaseContext base " + base);
+        super.attachBaseContext(base);
+    }
+
+    @Override
     public void onCreate() {
+        Log.i("xiabo", "XApplication onCreate");
         super.onCreate();
     }
 }
